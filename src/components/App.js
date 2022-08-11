29f565aef144d0674f26056087b323056531a8e9
Badi-Haki Martin <jojackblack@gmail.com>
2022-08-11 02:39:26 -0400

    changes "switch" to "routes"

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,32 @@
 import logo from '../logo.svg';
 import '../App.css';
 import React, { useState, useEffect } from 'react';
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import NavigationBar from './NavigationBar';
 import Home from './Home';
+import RestaurantList from './RestaurantList';
+import BartenderList from './BartenderList';
+import CocktailList from './CocktailList';
 
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
-      <Switch>
-        <Route exact path={'/'}>
+      <Routes>
+        <Route path={'/'}>
           <Home />
         </Route>
-      </Switch>
+        <Route path={'/restaurants'}>
+          <RestaurantList />
+        </Route>
+        <Route path={'/bartenders'}>
+          <BartenderList />
+        </Route>
+        <Route path={'/cocktails'}>
+          <CocktailList />
+        </Route>
+      </Routes>
     </div>
   );
 }
