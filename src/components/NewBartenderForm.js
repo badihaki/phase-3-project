62de10bcb1fe2eb9245b32648c47777df80c4a7e
Badi-Haki Martin <jojackblack@gmail.com>
2022-08-18 02:30:53 -0400

    updated NewBartenderForm.js, now actually has a state-controlled form

--- src/components/NewBartenderForm.js
+++ src/components/NewBartenderForm.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NewBartenderForm(){
+    const [bartenderForm, setBartenderForm] = useState(
+        {
+            "name": "",
+            "handle": "",
+            "restaurant_id":null
+        }
+    )
+    
     return(
         <div>
             <p>Add a new bartender to the registry using the form provided below.</p>
             <br/>
+            <form>
+                <input type={"text"} name={"name"} placeholder={"Bartender Name"} value={bartenderForm.name} />
+                <input type={"text"} name={"handle"} placeholder={"Nickname"} value={bartenderForm.handle} />
+                <br />
+                <button type='submit'>Create new Bartender</button>
+            </form>
         </div>
     )
 }
