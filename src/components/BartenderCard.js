10c60fae825a7d89240a0b146eb9c6d3ef02df57
Badi-Haki Martin <jojackblack@gmail.com>
2022-08-18 14:13:48 -0400

    fixed bug with BartenderCard.js where a bartender with no restaurant_id would not render correctly, causing an app-crashing failure. Now I am using a const called hasRestaurant to check if the bartender has has a restaurant_id

--- src/components/BartenderCard.js
+++ src/components/BartenderCard.js
@@ -1,6 +1,10 @@
 import React, {useState} from "react";
 
 function BartenderCard({ bartender, restaurant, cocktails }){
+    const hasRestaurant = ()=>{
+        return bartender.restaurant_id !== null;
+    }
+
     const cocktailListings = cocktails.map(cocktail=>{
         return(
             <li key={cocktail.id}>
@@ -12,9 +16,10 @@ function BartenderCard({ bartender, restaurant, cocktails }){
     })
     return(
         <div>
+            {console.log(bartender)}
             <h1>{bartender.name}</h1>
             <h2>They call me "{bartender.handle}"</h2>
-            <h3>See me at {restaurant.name}!</h3>
+            <h3>{hasRestaurant? "I'm looking for work!!" : `See me at ${restaurant.name}` }!</h3>
             <p style={{fontWeight:'bold'}} >Created Cocktails</p>
             <ul>{cocktailListings}</ul>
         </div>
