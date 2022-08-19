4d35f92491b1ecb70609d93bf78fc0b23d4722d1
Badi-Haki Martin <jojackblack@gmail.com>
2022-08-19 00:55:43 -0400

    updated CocktailForm.js to be state-controlled. Made it more user-friendly with explanations

--- src/components/CocktailForm.js
+++ src/components/CocktailForm.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 function CocktailForm({ bartender }){
+    const [newCocktilForm, setNewCocktailForm] = useState({
+        "name":"",
+        "description":"",
+        "bartender_id":bartender.id
+    })
+
+    function handleFormChange(event){
+        const key = event.target.name;
+        const value = event.target.value;
+        const formChange = {...newCocktilForm};
+        formChange[key] = value;
+        setNewCocktailForm(formChange);
+    }
+
     return(
         <form>
             <h3>Create a new cocktail</h3>
             <br />
-            <input type={text} name={"name"} />
-            <input type={text} name={"descripion"} />
+            <h4>Give the cocktail a name</h4>
+            <input type={text} name={"name"} placeholder={"Cocktail Name"} onChange={handleFormChange} />
+            <br />
+            <h4>And give it a description (ingredients, etc)</h4>
+            <input type={text} name={"descripion"} placeholder={"Cocktail Description"} onChange={handleFormChange} />
         </form>
     )
 }
